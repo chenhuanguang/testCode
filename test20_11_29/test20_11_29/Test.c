3bf0f0c44678ee21493407c40a3625d29083912a
BlackChg <2279295980@qq.com>
2020-11-29 19:49:46 +0800

    add Code

--- test20_11_29/test20_11_29/Test.c
+++ test20_11_29/test20_11_29/Test.c
@@ -4,6 +4,52 @@
 #include<string.h>
 #include<malloc.h>
 
+#define ROW 5
+#define COW 5
+
+int main()
+{
+	int **par = NULL;
+	par = (int**)malloc(sizeof(int*)*ROW);
+	if (NULL == par)
+	{
+		return;
+	}
+	for (int i = 0; i < ROW; i++)
+	{
+		*(par+i) = (int*)malloc(sizeof(int)*COW);
+		if (NULL == *(par + i))
+		{
+			return;
+		}
+	}
+
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COW; j++)
+		{
+			par[i][j] = i * j;
+		}
+	}
+
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COW; j++)
+		{
+			printf("%d \n", par[i][j]);
+		}
+	}
+
+	for (int i = 0; i < ROW; i++)
+	{
+		free(*(par + i));
+	}
+	free(par);
+
+	return 0;
+}
+
+/*
 int main()
 {
 	int *ar = (int *)malloc(sizeof(int) * 5);
@@ -11,7 +57,7 @@ int main()
 
 	return 0;
 }
-
+*/
 /*
 int main()
 {
