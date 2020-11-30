4f4015ecd9b41af135c897fac37bd0cfe45cbeb0
BlackChg <2279295980@qq.com>
2020-11-30 19:35:04 +0800

    20_11_30代码

--- Test20_11_30/Test20_11_30/Test.c
+++ Test20_11_30/Test20_11_30/Test.c
@@ -3,6 +3,73 @@
 #include<stdio.h>
 #include<malloc.h>
 
+typedef struct Test
+{
+	int a;
+	double b;
+	int c;
+	int d;
+	char *ch;
+}Test;
+
+int main()
+{
+	char str[] = "hello world";
+	Test *pt = (Test*)malloc(sizeof(Test));
+	if (NULL == pt)
+	{
+		printf("Insufficient memory available\n");//没有足够的空间
+		return;
+	}
+
+	pt->ch = (char*)malloc(strlen(str) + 1);
+	if (NULL == pt->ch)
+	{
+		printf("Insufficient memory available\n");//没有足够的空间
+		return;
+	}
+	strcpy(pt->ch, str);
+	printf("str = %s\n", pt->ch);
+
+	free(pt->ch);
+	pt->ch = NULL;
+
+	free(pt);
+	pt = NULL;
+
+	return 0;
+}
+
+/*
+typedef struct Test
+{
+	int a;
+	double b;
+	int c;
+	int d;
+	char ch[0];
+}Test;
+
+int main()
+{
+	char str[] = "hello world";
+	Test *pt = (Test*)malloc(sizeof(Test) + strlen(str) + 1);
+	if (NULL == pt)
+	{
+		printf("Insufficient memory available\n");//没有足够的空间
+		return;
+	}
+	
+	strcpy(pt->ch, str);
+	printf("str = %s\n", pt->ch);
+
+	free(pt);
+	pt = NULL;
+
+	return 0;
+}
+*/
+/*
 int main()
 {
 	int num = 10;
@@ -43,7 +110,7 @@ int main()
 
 	return 0;
 }
-
+*/
 /*
 int main()
 {
