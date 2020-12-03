18042de6dcbc8ebb099dd45737819a5e47d4cc8d
BlackChg <2279295980@qq.com>
2020-12-03 22:00:58 +0800

    leetCode

--- test20_12_3/test20_12_3/Test.c
+++ test20_12_3/test20_12_3/Test.c
@@ -8,11 +8,11 @@ int int_qsort(void* a, void* b)
 	return *(int*)a - *(int*)b;
 }
 
-bool uniqueOccurrences(int* arr, int arrSize) 
+bool uniqueOccurrences(int* arr, int arrSize)
 {
 	int j = 0;
 	int flag = 0;
-	qsort(arr, arrSize, sizeof(int), int_qsort); 
+	qsort(arr, arrSize, sizeof(int), int_qsort);
 
 	if (arrSize == 2)
 	{
@@ -26,41 +26,36 @@ bool uniqueOccurrences(int* arr, int arrSize)
 		}
 	}
 
-	for (int i = 0; i <= arrSize-1; i++)
+	for (int i = 0; i < arrSize - 1; i++)
 	{
-		if (i == arrSize -1)
-		{
-			arr[j++] = i - flag + 1;
-		}
-
 		if (arr[i] != arr[i + 1])
 		{
-			arr[j++] = i-flag + 1;	 ///1 1 1 2 2 3 3
-			flag = i+1; //5
-		}	
-		
+			arr[j++] = i - flag + 1;	 ///1 1 1 2 2 3
+			flag = i + 1;
+		}
 	}
+	arr[j] = 1;
 	qsort(arr, j, sizeof(int), int_qsort);
-	for (int i = 0; i < j-1; i++)
+	for (int i = 0; i < j - 1; i++)
 	{
 		if (arr[i] == arr[i + 1])
 		{
 			return false;
 		}
 	}
-	
+
 	return true;
 }
 
 //arr = [-3,0,1,-3,1,1,1,-3,10]
 int main()
 {
-	int ar[] = {1,2,2,1,1,3,3 };
+	int ar[] = { 1,2,2,1,1,3 };
 	int size = sizeof(ar) / sizeof(int);
 
 	bool flag = uniqueOccurrences(ar, size);
 
-	printf("%d\n",(int)flag);
+	printf("%d\n", (int)flag);
 
 	return 0;
 }
