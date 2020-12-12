d1c7187147b53030c3cbd64d1b9f43bd535b8908
BlackChg <2279295980@qq.com>
2020-12-12 15:01:20 +0800

    leetCode学生出勤记录

---
+++ Test12.12/Test12.12/test.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+//bool checkRecord(char * s)
+//{
+//	if (s == NULL)
+//		return false;
+//	int a = 0, l = 0;
+//	char *ps = s;
+//	char *ps2 = ps;
+//	while (*ps!='\0')
+//	{
+//		if (*ps == 'A')
+//		{
+//			a++;
+//		}
+//		if (a == 2)
+//			return false;
+//		if (*ps == 'L'&& *ps2!='L')
+//		{
+//			ps2 = ps;
+//			l++;
+//		}
+//		else if (*ps == 'L'&& *ps2 == 'L')
+//		{
+//			l++;
+//			if (l >= 3)
+//				return false;
+//		}
+//		if (*ps != 'L')
+//		{
+//			ps2 = ps;
+//			l = 0;
+//		}
+//			
+//		
+//		ps++;
+//	}
+//	return true;
+//}
+
+bool checkRecord(char * s)
+{
+	int len = strlen(s);
+
+	for (int i = 0; i < len - 2; ++i)
+	{
+		if (s[i] == 'L'&&s[i + 1] == 'L'&&s[i + 2] == 'L')
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	char *s = "LALL";
+	int flag = checkRecord(s);
+	printf("%d \n", flag);
+	return 0;
+}
+
+
+
