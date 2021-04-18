269664bd16196705fb9804b91a9c29955ba89dc8
BlackChg <2279295980@qq.com>
2021-04-18 21:52:05 +0800

    io基础

---
+++ testC++4.17/testC++4.17/test.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+
+
+//int main()
+//{
+//	//1、
+//	ofstream fout("test.txt");
+//	//2、
+//	//fout.open("test.txt");
+//	if (!fout.is_open())
+//	{
+//		cout << "file not open" << endl;
+//	}
+//	else
+//	{
+//		cout << "file opened" << endl;
+//	}
+//
+//	fout << "test.txt";
+//	fout.close();
+//
+//	//读文件：文件不存在则报错
+//	ifstream fin("test.txt");
+//
+//	char arr[100];
+//	//fin >> arr;
+//	cout << arr << endl;
+//	char ch;
+//	ch = fin.get();
+//	cout << ch << endl;
+//	return 0;
+//}
+
+//struct Student
+//{
+//	char name[10];
+//	int age;
+//};
+
+int main()
+{
+	//int a = 10;
+	//int b = 20;
+	//stringstream ss;
+	//string str;
+	//ss << a;
+	//ss >> str;
+
+	//ss.clear();
+	//ss << b;
+	//ss >> str;
+
+	////清空ss中的内容
+	//ss.str("");
+	////将ss中的内容写入到str中
+	//str = ss.str();
+
+	//ss.clear();
+	//ss << a;
+	//str = ss.str();
+	/*int a = 10;
+	double b= 3.14;
+	cout << 'A' + a + b << endl;*/
+	int x; 
+	int y;
+	for (x = 0, y = 0; (y = 123) && (x < 4); x++)
+	{
+
+	}
+	return 0;
+}
