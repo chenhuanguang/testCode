1c62e8baea0600aa79a514cece7cbd91c2e721a0
BlackChg <2279295980@qq.com>
2021-05-17 23:18:31 +0800

    avl树完整实现

--- AVL树/AVL树/AvlTree.cpp
+++ AVL树/AVL树/AvlTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 template<class T>
@@ -80,7 +81,47 @@ public:
 					//右单旋
 					RotateR(parent);
 				}
-				//...
+				else if (parent->_bf == 2 && cur->_bf == 1)//右边的右边高
+				{
+					//左单旋
+					RotateL(parent);
+				}
+				else if (parent->_bf == -2 && cur->_bf == 1) //左边的右边高
+				{
+					//左右双旋
+					Node* subLR = cur->_right;
+					int bf = subLR->_bf;
+					RotateL(cur);
+					RotateR(parent);
+					if (bf == -1)
+					{
+						parent->_bf = 1;
+						cur->_bf = 0;
+					}
+					if (bf == 1)
+					{
+						parent->_bf = 0;
+						cur->_bf = -1;
+					}
+				}
+				else if(parent->_bf == 2 && cur->_bf == -1) //右边的左边高
+				{
+					//右左单旋时
+					Node* subRL = cur->_left;
+					int bf = subRL->_bf;
+					RotateR(cur);
+					RotateL(parent);
+					if (bf == 1)
+					{
+						parent->_bf = -1;
+						cur->_bf = 0;
+					}
+					if (bf == -1)
+					{
+						parent->_bf = 0;
+						cur->_bf = 1;
+					}
+				}
 				break;
 			}
 		}
@@ -117,33 +158,161 @@ public:
 
 		subL->_bf = parent->_bf = 0;
 	}
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+			subRL->_parent = parent;
+		if (parent == _root)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			Node* pparent = parent->_parent;
+			if (pparent->_left == parent)
+				pparent->_left = subR;
+			else
+				pparent->_right = subR;
+			subR->_parent = pparent;
+		}
+		subR->_left = parent;
+		parent->_parent = subR;
+
+		subR->_bf = parent->_bf = 0;
+	}
+
+	void inorder()
+	{
+		_inorder(_root);
+		cout << endl;
+	}
+
+	void _inorder(Node* root)
+	{
+		if (root)
+		{
+			_inorder(root->_left);
+			cout << root->_val << " ";
+			_inorder(root->_right);
+		}
+	}
+
+	int height(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+		int left = height(root->_left);
+		int right = height(root->_right);
+
+		return left > right ? left + 1 : right + 1;
+	}
+
+	bool _isBalance(Node* root)
+	{
+		if (root == nullptr)
+			return true;
+
+		//查看平衡因子是否和左右子树的高度差一致
+		int left = height(root->_left);
+		int right = height(root->_right);
+
+		if (right - left != root->_bf || abs(root->_bf) > 1)//不相等或者平衡因子有误则表示不是AVL树
+			return false;
+		return _isBalance(root->_left) && _isBalance(root->_right);
+	}
+
+	bool isBalance()
+	{
+		cout << endl;
+		return _isBalance(_root);
+	}
 private:
 	Node* _root = nullptr;
 };
 
 void test()
 {
+	srand(time(nullptr));
+	cout << "num : ";
+	int num;
+	cin >> num;
 	AVLTreeNode<int> avl;
-	avl.insert(5);
-	avl.insert(3);
-	//    5
-	// 3
-	avl.insert(1); //右单旋
-	//    3
-	// 1     5 
-	avl.insert(0);
-	avl.insert(2);
-	avl.insert(-1);//右单旋
-	//右旋前
-	//       3
-	//    1      5
-	//  0  2
-	//-1
-	//右旋后
-	//       1
-	//    0      3
-	// -1      2   5
+	for (int i = 0; i < num; ++i)
+	{
+		avl.insert(rand());
+	}
+	avl.inorder();
+	cout << avl.isBalance() << endl;
 }
+
+//void test()
+//{
+//	AVLTreeNode<int> avl;
+//	avl.insert(5);
+//	avl.insert(3);
+//	avl.insert(1); //右单旋
+//	avl.insert(0);
+//	avl.insert(2);
+//	avl.insert(-1);//右单旋
+//	//       1
+//	//    0      3
+//	// -1      2   5
+//	avl.insert(10);
+//	avl.insert(15); //左单旋
+//	//左单旋前
+//	//       1
+//	//    0      3
+//	// -1      2   5
+//	//               10
+//	//                  15
+//	//左单旋后
+//	//       1
+//	//    0      3
+//	// -1      2    10
+//	//             5  15
+//	avl.insert(20);//左单旋
+//	//左单旋前
+//	//       1
+//	//    0      3
+//	// -1      2    10
+//	//            5    15
+//	//                   20
+//	//左单旋后
+//	//       1
+//	//    0      10
+//	// -1      3    15
+//	//       2   5     20
+//	avl.inorder();
+//}
+
+//void test()
+//{
+//	AVLTreeNode<int> avl;
+//	avl.insert(5);
+//	avl.insert(3);
+//	//    5
+//	// 3
+//	avl.insert(1); //右单旋
+//	//    3
+//	// 1     5 
+//	avl.insert(0);
+//	avl.insert(2);
+//	avl.insert(-1);//右单旋
+//	//右旋前
+//	//       3
+//	//    1      5
+//	//  0  2
+//	//-1
+//	//右旋后
+//	//       1
+//	//    0      3
+//	// -1      2   5
+//}
 
 int main()
 {
