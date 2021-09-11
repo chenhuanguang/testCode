534cd55b173a118e6c951541f0b3a7a1c7f2169a
BlackChg <2279295980@qq.com>
2021-09-11 23:59:25 +0800

    测试

---
+++ test8.24x/test8.24x/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <thread>
+#include <atomic>
+atomic_long sum{ 0 };
+void fun(size_t num) {
+	for (size_t i = 0; i < num; ++i)
+		sum++; // Ô­×Ó²Ù×÷
+}
+int main()
+{
+	cout << "Before joining, sum = " << sum << std::endl;
+	thread t1(fun, 1000000);
+	thread t2(fun, 1000000);
+	t1.join();
+	t2.join();
+
+	cout << "After joining, sum = " << sum << std::endl;
+	return 0;
+}
